8c275db4b23df41a8be899df8330e946f32d1a37
Mark Edwards <earth2mark@gmail.com>
2022-04-20 09:36:09 +0000

    Remove support for python 3.6

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ CLASSIFIERS = [
     "License :: OSI Approved :: BSD License",
     "Operating System :: OS Independent",
     "Programming Language :: Python",
-    "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
@@ -39,7 +38,6 @@ setup(
     include_package_data=True,
     install_requires=[
         "ariadne>=0.15.0",
-        "dataclasses; python_version < '3.7.0'",
         "graphql-relay>=3.2.0",
     ],
     extras_require={},
